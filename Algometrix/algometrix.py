426527d02f57858f8026e6b9ce6860df062467f2
ryyhan <dayel.rehan@gmail.com>
2023-09-01 16:31:52 +0530

    updated

--- Algometrix/algometrix.py
+++ Algometrix/algometrix.py
@@ -1,1 +1,21 @@
-...
+import numpy as np
+import pandas as pd
+from problem_types import Ptype
+from regression_models import regression_models
+from classification_models import classification_models
+
+def algometrix(X_train, X_test, y_train, y_test, prob_type="none", algorithms="none", metrics="none", cross_validation=False):
+    
+    algorithm_selection(prob_type,algorithms)
+
+
+def algorithm_selection(prob_type,algorithms):
+
+    prob_type = Ptype(prob_type)
+    print(prob_type)
+    print(algorithms)
+
+    if prob_type == "reg":
+        regression_models(algorithms)
+    elif prob_type == "class":
+        classification_models(algorithms)
