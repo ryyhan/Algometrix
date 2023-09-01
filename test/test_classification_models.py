cf0e4a262338e7008e3dfadf1a783e5a11065635
ryyhan <dayel.rehan@gmail.com>
2023-09-01 16:29:55 +0530

    classification model tests

---
+++ test/test_classification_models.py
@@ -0,0 +1,14 @@
+import pytest
+from Algometrix.classification_models import validation
+
+def test_validation():
+    assert validation("all") == "OK"
+    assert validation(["SVC"])
+    assert validation(['AdaBoostClassifier','BaggingClassifier','BernoulliNB','CalibratedClassifierCV','GaussianNB',]) == "OK"
+
+    with pytest.raises(TypeError):
+        validation("")
+        validation("SVC")
+        validation("Linear")
+        validation(["svc"])
+        validation(['AdaBoostClassifier','BaggingClassifier','BernoulliNB','CalibratedClassifierCV','GaussianNB',"sdc"])
