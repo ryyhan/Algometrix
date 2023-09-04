083c4a5ff93483f57212b22c04262b9b29eb3110
ryyhan <dayel.rehan@gmail.com>
2023-09-04 20:09:43 +0530

    support for binary/multiclass classification added

--- Algometrix/algometrix.py
+++ Algometrix/algometrix.py
@@ -11,14 +11,15 @@ def algometrix(
     y_train,
     y_test,
     prob_type="none",
+    classification_type="none",
     algorithms="none",
     metrics="none",
     cross_validation=False,
 ):
-    algorithm_selection(X_train, X_test, y_train, y_test, prob_type, algorithms)
+    algorithm_selection(X_train, X_test, y_train, y_test, prob_type, algorithms, classification_type)
 
 
-def algorithm_selection(X_train, X_test, y_train, y_test, prob_type, algorithms):
+def algorithm_selection(X_train, X_test, y_train, y_test, prob_type, algorithms, classification_type):
     prob_type = Ptype(prob_type)
     print(prob_type)
     print(algorithms)
@@ -26,4 +27,4 @@ def algorithm_selection(X_train, X_test, y_train, y_test, prob_type, algorithms)
     if prob_type == "reg":
         regression_models(X_train, X_test, y_train, y_test, algorithms)
     elif prob_type == "class":
-        classification_models(X_train, X_test, y_train, y_test, algorithms)
+        classification_models(X_train, X_test, y_train, y_test, algorithms,classification_type)
